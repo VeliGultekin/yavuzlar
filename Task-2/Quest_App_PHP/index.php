dfe57187faf175514e8fedad489fe6f12771af51
VeliGultekin <120390428+VeliGultekin@users.noreply.github.com>
2024-09-14 22:52:50 +0300

    Updated index.php

--- Task-2/Quest_App_PHP/index.php
+++ Task-2/Quest_App_PHP/index.php
@@ -3,33 +3,47 @@
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
-    header('Location: login.php'); 
+    header('Location: login.php');
     exit;
 }
 $user_role = $_SESSION['user_role'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anasayfa</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <div class="container">
 
-    <button onclick="admin()">Admin Paneli</button>
-    <br>
-    <?php if ($user_role === 'admin'): ?>
-    <button onclick="soru()">Quiz'e Başla</button>
-    <br>
-    <?php endif; ?>
-    <button onclick="score()">Scoreboard</button>
-    <br>
-    <button onclick="logout()">Çıkış Yap</button>
-    <br>
+
+        <?php if ($user_role === 'admin'): ?>
+            <button onclick="admin()">Admin Paneli</button>
+            <br>
+            <button onclick="soru()">Quiz'e Başla</button>
+            <br>
+
+            <button onclick="score()">Scoreboard</button>
+            <br>
+            <button onclick="logout()">Çıkış Yap</button>
+            <br> <?php endif; ?>
+
+        <?php if ($user_role === 'user'): ?>
+            <button onclick="soru()">Quiz'e Başla</button>
+            <br>
+
+            <button onclick="score()">Scoreboard</button>
+            <br>
+            <button onclick="logout()">Çıkış Yap</button>
+            <br>
+        <?php endif; ?>
     </div>
     <script src="script.js"></script>
 </body>
+
 </html>
